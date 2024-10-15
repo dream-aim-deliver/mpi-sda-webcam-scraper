caf6b1c2c240697739a619dc20d80c11976445a2
Rahul Aryan <rahularyan7131@gmail.com>
2024-10-15 06:26:12 +0530

    Update demo_URL.sh

--- demo_URL.sh
+++ demo_URL.sh
@@ -3,6 +3,6 @@ python webcam_scraper.py --url "http://131.147.222.67:8089/?action=stream" \
     --duration "60" \
     --latitude " 35.652832" \
     --longitude "139.839478" \
-    --date "10/10/24" \
+    --date "15-10-24" \
     --log-level="INFO" \
     --kp_auth_token test123 --kp_host localhost --kp_port 8000 --kp_scheme http \
