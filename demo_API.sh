7316be4133b66e1ea4a46c950064ac9d80d6ab6b
Rahul Aryan <rahularyan7131@gmail.com>
2024-10-15 06:15:07 +0530

    Update demo_API.sh

--- demo_API.sh
+++ demo_API.sh
@@ -1,2 +1,3 @@
-python webcam_scraper.py --latitude "51.504105" --longitude "-0.074575" --start_date "2024-09-06" --end_date "2024-09-06" --log-level="INFO" \
+python API_scraper.py --latitude "35.652832" --longitude "139.839478" \
+ --start_date "2024-10-12" --end_date "2024-10-15" --log-level="INFO" \
  --kp_auth_token test123 --kp_host localhost --kp_port 8000 --kp_scheme http \
