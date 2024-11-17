9ece9a03712cf54a49559711b3218fad5147c3b5
Luis Alejandro Bordo García <bgluiszz@gmail.com>
2024-11-17 01:32:52 +0100

    fix: time interval logic; further cleanup of error handling

--- app/url_image_scraper.py
+++ app/url_image_scraper.py
@@ -1,6 +1,6 @@
 from datetime import datetime,timedelta
 from app.sdk.models import KernelPlancksterSourceData, BaseJobState, JobOutput
-from app.sdk.scraped_data_repository import KernelPlancksterSourceData
+from app.sdk.scraped_data_repository import KernelPlancksterSourceData, ScrapedDataRepository
 import time
 import numpy as np
 from typing import List
@@ -19,20 +19,20 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-def fetch_images_from_url(url: str, date: datetime) -> Image.Image:
-    
-    split_url = url.split('/')
-    webcam_id = split_url[3]
-    url_template = "https://storage.roundshot.com/{webcam_id}/{year}-{month}-{day}/{hour}-{minute}-00/{year}-{month}-{day}-{hour}-{minute}-00_half.jpg"
+def fetch_images_from_url(url: str, date: datetime) -> Image.Image | None:
     
     try:
+        split_url = url.split('/')
+        webcam_id = split_url[3]  # NOTE: Might as well pass a webcam_id to the function directly
+        url_template = "https://storage.roundshot.com/{webcam_id}/{year}-{month}-{day}/{hour}-{minute}-00/{year}-{month}-{day}-{hour}-{minute}-00_half.jpg"
+        
         url = url_template.format(
                 webcam_id=webcam_id,
                 year=date.year,
                 month=f"{date.month:02}",
                 day=f"{date.day:02}",
-                hour=f"{date.hour:02}" if date >= datetime.now() else "12",
-                minute=f"{date.minute:02}" if date >= datetime.now() else "00",
+                hour=f"{date.hour:02}",
+                minute=f"{date.minute:02}",
             )
         logger.info(f"Fetching image from: {url}")
             
@@ -42,10 +42,13 @@ def fetch_images_from_url(url: str, date: datetime) -> Image.Image:
             
         # Convert the response content to a PIL Image
         image = Image.open(BytesIO(response.content))
-    except Exception as e:
-        logger.error(f"Error fetching image for {date}: {e}")
         
-    return image
+        return image
+    
+    except Exception as e:
+        logger.warning(f"Unable to fetch image from '{url}'. Error: {e}")
+        return None
+
 
 def save_image(image, path, factor=1.0, clip_range=(0, 1)):
     """Save the image to the given path."""
@@ -54,120 +57,109 @@ def save_image(image, path, factor=1.0, clip_range=(0, 1)):
     np_image = (np_image * 255).astype(np.uint8)
     Image.fromarray(np_image).save(path)
 
+
 # Updated scrape_URL function
-def scrape_URL(case_study_name, job_id, tracer_id, scraped_data_repository, log_level, latitude, longitude, start_date: datetime, end_date: datetime, file_dir, url, interval):
+def scrape_URL(case_study_name: str, job_id: int, tracer_id: str, scraped_data_repository: ScrapedDataRepository, log_level: str, latitude, longitude, start_date: datetime, end_date: datetime, file_dir: str, url: str, interval: int) -> JobOutput:
+
+    job_state = BaseJobState.CREATED
+
+    start_time = time.time()
+
     try:
         logger = logging.getLogger(__name__)
         logging.basicConfig(level=log_level)
 
-        job_state = BaseJobState.CREATED
-        current_data: KernelPlancksterSourceData | None = None
-        last_successful_data: KernelPlancksterSourceData | None = None
-
         protocol = scraped_data_repository.protocol
 
         output_data_list: List[KernelPlancksterSourceData] = []
-        if job_state:  # for typing
-            logger.info(f"{job_id}: Starting Job")
-            job_state = BaseJobState.RUNNING
+
+        logger.info(f"{job_id}: Starting Job")
+
+        job_state = BaseJobState.RUNNING
+
+        logger.info(f"starting with webcam URL")
+        image_dir = os.path.join(file_dir, "images")
+        os.makedirs(image_dir, exist_ok=True)
+        current_date = start_date
+        interval = timedelta(minutes=interval)# if start_date == datetime.now() else timedelta(days=1)
+        #iteration=0
+        logger.info(f"Data scraping Interval set at: {interval}")
+
+        while (current_date <= end_date):
             try:
-                logger.info(f"starting with webcam URL")
-                image_dir = os.path.join(file_dir, "images")
-                os.makedirs(image_dir, exist_ok=True)
-                current_date = start_date
-                interval = timedelta(minutes=interval) if start_date == datetime.now() else timedelta(days=1)
-                iteration=0
-                logger.info(f"Data scraping Interval set as:{interval}")
-                while (current_date <= end_date):
-                    image = fetch_images_from_url(url=url,date=current_date)
+                image = fetch_images_from_url(url=url, date = current_date)
 
-                    if image is None:
-                        logger.error("Error: Unable to fetch image.")
-                        continue
-                    if (current_date <= end_date) and (np.mean(image) != 0.0):  
+                if image is None:
+                    current_date += interval
+                    continue  # logging is done in fetch_images_from_url
 
-                        # Save the image locally
-                        file_extension = image.format.lower() if image.format else "png"
-                        image_filename = f"URLbased_webcam.{file_extension}"
-                        unix_timestamp = int(current_date.timestamp()) + iteration   
-                        image_path = os.path.join(image_dir, "scraped", image_filename)
-                        os.makedirs(os.path.dirname(image_path), exist_ok=True)
-                        save_image(image, image_path, factor=1.5 / 255, clip_range=(0, 1))
-                        logger.info(f"Scraped Image at {time.time()} and saved to: {image_path}")
+                if (current_date <= end_date) and (np.mean(image) != 0.0):  
 
-                        # Register it in Kernel Planckster
-                        data_name = f"webcam_{latitude}_{longitude}"
+                    # Save the image locally
+                    file_extension = image.format.lower() if image.format else "png"
+                    image_filename = f"URLbased_webcam.{file_extension}"
+                    unix_timestamp = int(current_date.timestamp())
+                    image_path = os.path.join(image_dir, "scraped", image_filename)
+                    os.makedirs(os.path.dirname(image_path), exist_ok=True)
+                    save_image(image, image_path, factor=1.5 / 255, clip_range=(0, 1))
+                    logger.info(f"Scraped Image at {time.time()} and saved to: {image_path}")
 
-                        relative_path = f"{case_study_name}/{tracer_id}/{job_id}/{unix_timestamp}/webcam/{data_name}.{file_extension}"
+                    # Register it in Kernel Planckster
+                    data_name = f"webcam_{case_study_name}_{tracer_id}"
 
-                        current_date += interval
-                        iteration += 1
-                        
-                        media_data = KernelPlancksterSourceData(
-                            name=data_name,
-                            protocol=protocol,
-                            relative_path=relative_path,
-                        )
-                        
-                        try:
-                            scraped_data_repository.register_scraped_photo(
-                                job_id=job_id,
-                                source_data=media_data,
-                                local_file_name=image_path,
-                            )
-                        except Exception as e:
-                            logger.info(f"Could not register file: {e}")
-                        
-                        print(f"job_id = {job_id} and tracer_id = {tracer_id}")
-                        response_time = time.time() - start_date.timestamp()
-                        response_data = {
-                            "message": f"Pipeline processing completed",
-                            "response_time": f"{response_time:.2f} seconds"
-                        }
+                    relative_path = f"{case_study_name}/{tracer_id}/{job_id}/{unix_timestamp}/webcam/{data_name}.{file_extension}"
 
-                        output_data_list.append(media_data)
+                    current_date += interval
+                    
+                    media_data = KernelPlancksterSourceData(
+                        name=data_name,
+                        protocol=protocol,
+                        relative_path=relative_path,
+                    )
+                    
+                    scraped_data_repository.register_scraped_photo(
+                        job_id=job_id,
+                        source_data=media_data,
+                        local_file_name=image_path,
+                    )
+
+                    output_data_list.append(media_data)
+
+                    time.sleep(0.1)   
+
+            except Exception as e:
+                logger.warning(f"Could not scrape data for {current_date}: {e}")
                 time.sleep(0.1)   
-                
-                job_state = BaseJobState.FINISHED
-                logger.info(f"{job_id}: Job finished")
-                
-                return JobOutput(
-                    job_state=job_state,
-                    tracer_id=tracer_id,
-                    source_data_list=output_data_list
-                )
+                continue
+                    
 
-            except Exception as e:
-                logger.error(f"Error in processing pipeline: {e}")
-                job_state = BaseJobState.FAILED
-                f"{job_id}: Unable to scrape data. Error:\n{e}\nJob with tracer_id {tracer_id} failed.\nLast successful data: {last_successful_data}\nCurrent data: \"{current_data}\", job_state: \"{job_state}\""
-                
-                return JobOutput(
-                    job_state=job_state,
-                    tracer_id=tracer_id,
-                    source_data_list=[]
-                )
-
-        if os.path.exists(file_dir):
-            try:
-                shutil.rmtree(file_dir)
-                print("deleted tmp directory, exiting")
-            except Exception as e:
-                logger.warning(f"Could not delete tmp directory,due to error: '{e}'")
-            else:
-                logger.warning(f"Temporary directory '{file_dir}' does not exist, skipping deletion.")
-                
+        response_time = time.time() - start_time
+        logger.info(f"{job_id}: Job finished successfully. Response time: {response_time:.2f} seconds")
+        
         return JobOutput(
-                job_state=job_state,
-                tracer_id=tracer_id,
-                source_data_list=output_data_list
-            )
+            job_state=BaseJobState.FINISHED,
+            tracer_id=tracer_id,
+            source_data_list=output_data_list
+        )
+
 
     except Exception as error:
-        logger.error(f"{job_id}: Unable to scrape data. Job with tracer_id {tracer_id} failed. Error:\n{error}")
-        job_state = BaseJobState.FAILED
+        logger.error(f"{job_id}: Unable to scrape data. Job with tracer_id {tracer_id} failed. Job state was '{job_state.value}' Error: {error}")
+
+        return JobOutput(
+            job_state=BaseJobState.FAILED,
+            tracer_id=tracer_id,
+            source_data_list=[]
+        )
+    
+    finally:
         try:
-            logger.warning("deleting tmp directory")
-            shutil.rmtree(file_dir)
+            if os.path.exists(file_dir):
+                shutil.rmtree(file_dir)
+                logger.info(f"Deleted tmp directory '{file_dir}'.")
+            else:
+                logger.info(f"Temporary directory '{file_dir}' does not exist, skipping deletion.")
+
         except Exception as e:
-            logger.warning("Could not delete tmp directory, exiting")
+            logger.warning("Could not delete tmp directory, exiting.")
+        
--- demo_URL.sh
+++ demo_URL.sh
@@ -1,11 +1,10 @@
-python webcam_scraper.py --url "https://storage.roundshot.com/54ae54684746d3.82338131/2024-11-17/03-00-00/2024-11-17-03-00-00_half.jpg" \
-    --start_date "2023-10-15T22:00" \
-    --end_date "2023-10-20T23:00" \
-    --interval "30" \
+python webcam_scraper.py --url "https://storage2.roundshot.com/5e568898681458.46669392/2024-11-16/13-00-00/2024-11-16-13-00-00_half.jpg" \
+    --start_date "2024-09-15T09:00" \
+    --end_date "2024-09-15T12:00" \
+    --interval "60" \
     --latitude "35.652832" \
     --longitude "139.839478" \
-    --date "15-10-24" \
     --log-level="INFO" \
     --case-study-name "disaster_tracker" \
-    --tracer-id "test" --job-id "1" \
+    --tracer-id "argentina" --job-id "1" \
     --kp_auth_token test123 --kp_host localhost --kp_port 8000 --kp_scheme http 
